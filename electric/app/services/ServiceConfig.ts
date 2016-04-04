dcea1093aa2a2d456a60d4606edb1f5a10343ec8
Vaughan Hilts <hilts.vaughan@gmail.com>
2016-04-04 01:47:58 -0400

    updated electric service URL

--- electric/app/services/ServiceConfig.ts
+++ electric/app/services/ServiceConfig.ts
@@ -3,7 +3,7 @@ export class ServiceConfig {
    * A static location to the server API endpoint
    * @type {String}
    */
-  static SERVER_URL = "http://localhost:8080"
+  static SERVER_URL = "http://159.203.11.151:8080"
 
   /**
    * The API key; this should be kept a secret usually, but we're fine here
