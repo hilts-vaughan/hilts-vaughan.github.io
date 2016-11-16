17e9497976abf6482b960f2d78d99065437a0405
Vaughan Hilts <hilts.vaughan@gmail.com>
2016-11-16 00:23:03 -0500

    removed electric

--- electric/app/main.ts
+++
@@ -1,9 +0,0 @@
-import {bootstrap} from 'angular2/platform/browser';
-import {HTTP_BINDINGS} from 'angular2/http';
-import {AppComponent} from './app.component';
-import 'rxjs/Rx'
-
-// Common bootstraps for many things
-import {NgFor, NgIf} from 'angular2/common'
-
-bootstrap(AppComponent, [HTTP_BINDINGS,]);
--- electric/app/models/ChargerType.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Represents a basic charging type available, which is eventually assigned back
- * to something else on the server.
- */
-export enum ChargerType {
-  Default,
-  Supercharger,
-  CHAMEDO
-}
