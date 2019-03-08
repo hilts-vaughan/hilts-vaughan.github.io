cde3e37954fdd2d790427522ff5fb8c69fe9d844
Vaughan Hilts <hilts.vaughan@gmail.com>
2019-03-07 23:10:35 -0500

    Updated w/ latest version of depends to make sure things are secure

---
+++ vendor/bundle/ruby/2.6.0/gems/jekyll-compose-0.9.0/lib/jekyll-compose/file_info.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module Compose
+    class FileInfo
+      attr_reader :params
+      def initialize(params)
+        @params = params
+      end
+
+      def file_name
+        name = Jekyll::Utils.slugify params.title
+        "#{name}.#{params.type}"
+      end
+
+      def content(custom_front_matter = {})
+        front_matter = YAML.dump({
+          "layout" => params.layout,
+          "title"  => params.title,
+        }.merge(custom_front_matter))
+
+        front_matter + "---\n"
+      end
+    end
+  end
+end
